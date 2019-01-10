e73fc39457c8def7a3d46d8ca42f8ddcc80968a3
igolinin@gmail.com <igolinin@gmail.com>
2019-01-10 05:16:00 +0100

    first commit

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -4,7 +4,7 @@ const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input {...rest} name={name} id={name} className="form-control w-50" />
+      <input {...rest} name={name} id={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
